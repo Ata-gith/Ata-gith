7929588a72f18cef0cff4eb082d35539cbf2f407
Ata <59917929+Ata-gith@users.noreply.github.com>
2022-05-04 15:10:41 +0300

    Add files via upload

---
+++ lammps_periodicity.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+
+int main()
+{
+
+int xmin,xmax,ymin,ymax,zmin,zmax;
+
+bool xper=true;
+bool yper=true;
+bool zper=true;
+
+std::std::vector<int> xcoor;
+std::std::vector<int> ycoor;
+std::std::vector<int> zcoor;
+
+
+int atom,group,masscoef,charge,x,y,z,nx,ny,nz;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ surface_maker.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+
+
+
+int main(int argc, char **argv)
+{
+
+
+double massC = 12.011;
+double massH = 1.008;
+
+char bondCC[5]= "gb_27";
+char bondCH[5]= "gb_3";
+char angleCCC[5]= "ga_"
+
+int coorx=0;coory=0;coorz=0;
+
+
+return 0;
+}
